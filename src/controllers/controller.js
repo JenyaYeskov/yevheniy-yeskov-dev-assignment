d75660598c8f66408321d0f958096dac3014d2fd
Jenya <jeskov95@gmail.com>
2022-12-03 23:04:39 +0200

    handle transactions according to transaction type with suitable method in the controller.js

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -2,7 +2,28 @@ import transactionsService from "../services/transactionsService.js";
 
 class Controller {
     async handleTransaction(req, res) {
-        res.send(await transactionsService.deposit());
+        for (const transaction of req.body) {
+            let type = transaction.type.toLowerCase().trim()
+
+            if (type === "deposit") {
+                res.send(await transactionsService.deposit())
+            }
+
+            if (type === "withdraw") {
+                res.send(await transactionsService.withdraw())
+            }
+
+            if (type === "buy") {
+                res.send(await transactionsService.buy())
+            }
+
+            if (type === "sell") {
+                res.send(await transactionsService.sell())
+            }
+
+            res.send("error")
+        }
     }
 }
+
 export default new Controller();
