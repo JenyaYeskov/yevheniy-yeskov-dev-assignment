5090da74017b6b7f07b1973bbd6fbef4816112b4
Jenya <jeskov95@gmail.com>
2022-12-04 01:16:21 +0200

    saving to transactions feature realization in the transactionsService.deposit()

--- src/services/transactionsService.js
+++ src/services/transactionsService.js
@@ -1,6 +1,10 @@
 class TransactionsService {
-    async deposit() {
+    async deposit(transaction) {
+        let {type, party, counterParty, assetType, amount} = transaction;
 
+        let time = new Date().toLocaleString();
+
+        return (await postgres.saveToTransactions([type, party, counterParty, assetType, amount, 0, amount, time]));
     }
 
     async withdraw() {
