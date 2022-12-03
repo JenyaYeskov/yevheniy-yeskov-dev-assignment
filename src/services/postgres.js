5749f75ccd7c84361c054addffc531b31ca435bb
Jenya <jeskov95@gmail.com>
2022-12-04 01:13:57 +0200

    saveToTransactions() method realization in the postgres.js

---
+++ src/services/postgres.js
@@ -0,0 +1,23 @@
+import pg from "pg";
+import "dotenv/config";
+
+const pool = new pg.Pool({
+    user: process.env.postgresUser,
+    host: process.env.postgresHost,
+    database: process.env.postgresDatabase,
+    password: process.env.postgresPassword,
+    port: process.env.postgresPort
+})
+
+class Postgres {
+    async saveToTransactions(data) {
+        let queryString = `insert into transactions (type, party, "counterParty", "assetType", amount, price, total, timestamp) 
+                            values ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *`;
+
+        let result = await pool.query(queryString, data);
+
+        return result.rows[0];
+    }
+}
+
+export default new Postgres();
