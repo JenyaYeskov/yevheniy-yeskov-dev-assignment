0f2b6f9376f95ecea29758dbdd0d3a2a00fd8515
Jenya <jeskov95@gmail.com>
2022-12-04 03:47:48 +0200

    check if accounts exist, and create new if not transactionsService.deposit()

--- src/services/transactionsService.js
+++ src/services/transactionsService.js
@@ -6,7 +6,19 @@ class TransactionsService {
 
         let time = new Date().toLocaleString();
 
-        return (await postgres.saveToTransactions([type, party, counterParty, assetType, amount, 0, amount, time]));
+        let partyData = await postgres.getAccountData(party);
+        let counterPartyData = await postgres.getAccountData(counterParty);
+
+        if (!partyData){
+            partyData = await postgres.createNewAccount([party, 0]);
+        }
+
+        if (!counterPartyData){
+            counterPartyData = await postgres.createNewAccount([counterParty, 0]);
+        }
+
+
+        return ("done");
     }
 
     async withdraw(transaction) {
