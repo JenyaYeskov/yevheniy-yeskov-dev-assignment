2dfaac369923584e3fdba23fea7eeba0ed6cc23e
Jenya <jeskov95@gmail.com>
2022-12-05 04:02:07 +0200

    getBelongings() method implementation in the logsService.js

--- src/services/logsService.js
+++ src/services/logsService.js
@@ -1,8 +1,42 @@
 import db from "./postgres.js";
+import Hstore from "pg-hstore";
+
+let hstore = new Hstore();
 
 class LogsService {
     async getBelongings(data) {
+        let result = await db.getPersonLogByDate(data.name, data.timestamp);
 
+        if (!result[0]) {
+            return;
+        }
+
+        let latest = result[result.length - 1];
+
+        for (const log of result) {
+            if (log.timestamp > latest.timestamp) {
+                latest = log.timestamp;
+            }
+        }
+
+        let assets = "";
+
+        if (latest.assets) {
+            latest.assets = hstore.parse(latest.assets);
+
+            for (let [key, value] of Object.entries(latest.assets)) {
+                if (value > 0) {
+                    value = "+" + value;
+                }
+                assets += `${value} ${key}, `;
+            }
+
+            assets = " | " + assets;
+            assets = assets.slice(0, -2);
+        }
+
+
+        return `${latest.money} €${assets}`;
     }
 }
 
